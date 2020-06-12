389219b85522e53168b06c91bbeea4cfba768b5d
slarge <largesi@gmail.com>
2020-06-12 15:41:48 -0400

    sentiment on 50.6 parts

--- analysis/paper/cfr_sentiment.R
+++ analysis/paper/cfr_sentiment.R
@@ -2,31 +2,40 @@ library(ggplot2)
 library(sentimentr)
 library(tidyr)
 library(dplyr)
+library(patchwork)
+library(magrittr)
 
 
 all_cfr <- readRDS("analysis/data/raw_data/20200609-cfrtext.rds")
 
-tt <- all_cfr %>%
-  head(1) %>%
+
+section_sentiment <- all_cfr %>%
   select(-year, -part) %>%
   tidyr::unnest(cols = c(regulations)) %>%
   tidyr::unnest(cols = c(data)) %>%
-  # group_by(SECTION_NUMBER) %>%
-  mutate(sentences = purrr::map(TEXT, get_sentences),
-         sentiment = purrr::map(sentences, sentiment_by)) %>%
-  unnest(cols = sentiment) %>%
+  mutate(sentences = get_sentences(TEXT)) %$%
+  sentiment_by(sentences, list(year, part)) %>%
   mutate(max = ave_sentiment + 2 * sd,
          min = ave_sentiment - 2 * sd) %>%
   I()
 
-tt %>%
-  ggplot() +
-  #aes(y = ave_sentiment, x = factor(year)) +
-  aes(y = ave_sentiment, ymin = min, ymax = max,  x = SECTION_NUMBER) +
-  geom_point() +
-  geom_linerange()
-  # geom_jitter(alpha = 0.5) +
-  # geom_smooth() +
-  NULL
+sect <- section_sentiment %>%
+   ggplot() +
+   aes(y = ave_sentiment, ymin = min, ymax = max,  x = as.factor(year), color = as.factor(part)) +
+   geom_point() +
+   geom_linerange() +
+   labs(title = "Title 50 Chapter 6",
+          subtitle = "Sentiment Analysis by part over time",
+        x = "",
+        y = "Mean sentiment (±2×sd)",
+        color = "") +
+   facet_wrap(.~ part) +
+   theme_bw() +
+   theme(axis.text.x = element_text(angle = 90, size = 8)) +
+   NULL
+
+ggsave("analysis/figures/sentiment_on_50.pdf", sect)
+
+
 
 
