2f997aa41687b822f3076256f62eb97ac37b23f2
slarge <largesi@gmail.com>
2020-10-28 13:35:51 -0400

    update functions with better checks

--- R/utilities.R
+++ R/utilities.R
@@ -6,12 +6,15 @@ get_section <- function(x) {
 }
 
 
-get_fr_summary <- function(fr_xml_url, cfr_part){
+get_fr_summary <- function(fr_xml_url, cfr_part, return_summary_text = FALSE){
 
   res <- xml2::read_xml(fr_xml_url, as = "parsed", encoding = "UTF-8")
 
   classification <- NA
   sectno <- NA
+  fr_summary <- NA
+  fr_supplement <- NA
+
   ## Capture all referenced section numbers from the Classification section of the supplimentary information
   if(length(xml2::xml_find_all(res, ".//SUPLINF/HD[contains(text(), 'Classification')]")) > 0) {
     regx <- sprintf("(%s\\.[[:digit:]]{1,4})", cfr_part)
@@ -31,6 +34,7 @@ get_fr_summary <- function(fr_xml_url, cfr_part){
       xml2::xml_text() %>%
       gsub("\u00A7|\u2009|\\s|and", "", .) %>%
       strsplit(., split = ",") %>% unlist()
+
     if(length(sectno) == 0){
       sectno <- NA
     }
@@ -39,21 +43,47 @@ get_fr_summary <- function(fr_xml_url, cfr_part){
   if(all(is.na(classification) & is.na(sectno))){
     warning(paste0("No CFR sections found in ", fr_xml_url, ". Part ", cfr_part))
   }
-  ## Extract the summary section
-  fr_summary <- res %>%
-    xml2::xml_find_all(".//SUM") %>%
-    xml2::xml_text() %>%
-    gsub("^SUMMARY:", "", .)
-  ## Extract the subbpementary information section
-  fr_supplement <- res %>%
-    xml2::xml_find_all(".//SUPLINF/HD[contains(text(), 'SUPPLEMENTARY INFORMATION')]/following-sibling::P") %>%
-    xml2::xml_text() %>%
-    gsub("^SUPPLEMENTARY INFORMATION:", "", .) %>%
-    stringr::str_c(collapse = " ")
 
-  summary_df <- tibble::tibble(cfr_section_number = c(classification, sectno), fr_summary, fr_supplement ) %>%
-    distinct(.keep_all = TRUE) %>%
-    na.omit(cfr_section_number)
+  if(return_summary_text) {
+    ## Extract the summary section
+    if(length(xml2::xml_find_all(res, ".//SUM")) > 0) {
+      fr_summary <- res %>%
+        xml2::xml_find_all(".//SUM") %>%
+        xml2::xml_text() %>%
+        gsub("^SUMMARY:", "", .)
+
+      if(length(fr_summary) == 0)
+        fr_summary <- NA
+    }
+
+    ## Extract the supplementary information section
+    if(length(xml2::xml_find_all(res, ".//SUPLINF/HD[contains(text(), 'SUPPLEMENTARY INFORMATION')]/following-sibling::P")) > 0) {
+      fr_supplement <- res %>%
+        xml2::xml_find_all(".//SUPLINF/HD[contains(text(), 'SUPPLEMENTARY INFORMATION')]/following-sibling::P") %>%
+        xml2::xml_text() %>%
+        gsub("^SUPPLEMENTARY INFORMATION:", "", .) %>%
+        stringr::str_c(collapse = " ")
+
+      if(fr_supplement == "") {
+        fr_supplement <- NA
+      }
+    }
+
+    ## Capture the warning output to go back and check on if there isn't CFR reference or if the reference is hiding in an unknown spot
+    if(all(is.na(fr_summary) & is.na(fr_supplement))){
+      warning(paste0("No 'Summary' or 'Supplementary Information' found in ", fr_xml_url, ". Part ", cfr_part))
+    }
+
+    summary_df <- tibble::tibble(cfr_section_number = c(classification, sectno), fr_summary, fr_supplement ) %>%
+      distinct(.keep_all = TRUE) %>%
+      filter(!is.na(cfr_section_number))
+  }
+  if(!return_summary_text){
+
+    summary_df <- tibble::tibble(cfr_section_number = c(classification, sectno), fr_summary, fr_supplement ) %>%
+      distinct(.keep_all = TRUE) %>%
+      filter(!is.na(cfr_section_number))
+  }
 
   return(summary_df)
 }
